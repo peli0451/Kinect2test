e0cbc3bbaa7ba1db15f8c1827eb8162101aca238
peli0451 <p.lindner@tu-ilmenau.de>
2017-04-29 16:50:43 +0200

    Hotfix für letzten Commit [kompiliert]

--- KinectControl/Person.h
+++ KinectControl/Person.h
@@ -75,7 +75,7 @@ private:
 
 	Limits bodyPropertiesLimits[NUMBER_OF_BODY_PROPERTIES];
 	float bodyProperties[NUMBER_OF_BODY_PROPERTIES] = { 0.f };
-	float bodyPropertiesWeights[6] = { 1.0f, 0.7f, 0.5f, 0.3f, 0.1, 0.0f };
+	float bodyPropertiesWeights[6] = { 1.0f, 0.7f, 0.5f, 0.3f, 0.1f, 0.0f };
 	int numberOfWeights;
 
 	int id;
